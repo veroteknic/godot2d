000e9134263e3a649e10dbdc5aa30c5c48d433ac
HaoyangLuDozCoding <106737917+HaoyangLuDozCoding@users.noreply.github.com>
2024-09-10 15:29:35 +0800

    fixed basic bugs

--- upwardplat.gd
+++ upwardplat.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 @onready var ray_cast_up = $RayCast2D
 @onready var ray_cast_down = $RayCast2D2
 var direction = 1
-const SPEED = 200
+const SPEED = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
